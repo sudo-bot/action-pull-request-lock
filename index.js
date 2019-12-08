484964f9e344cad7b10f3b5de9202d30c722687c
William Desportes <williamdes@wdes.fr>
2019-12-08 19:01:52 +0100

    Activate sailor-v preview on lock route
    Ref: https://developer.github.com/v3/issues/#lock-an-issue

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const core = require('@actions/core')
 const { GitHub, context } = require('@actions/github')
 
 const main = async () => {
-  const token = core.getInput('github-token');
-  const lock_reason = core.getInput('lock-reason');
-  const number = core.getInput('number');
+    const token = core.getInput('github-token');
+    const lock_reason = core.getInput('lock-reason');
+    const number = core.getInput('number');
 
-  const octokit = new GitHub(token);
+    const octokit = new GitHub(token);
 
-  await octokit.issues.lock({
-    lock_reason: lock_reason,
-    ...context.repo,
-    ...context.owner,
-    issue_number: number,
-  })
+    await octokit.issues.lock({
+        lock_reason: lock_reason,
+        ...context.repo,
+        ...context.owner,
+        issue_number: number,
+        mediaType: {
+            previews: [
+                'sailor-v'
+            ]
+        }
+    })
 }
 
 main().catch(err => core.setFailed(err.message))
